160114c88e2e772b2343e654c25768eafc984f78
yonmoyonmo <yonmosound@gmail.com>
2021-06-14 20:46:01 +0900

    진짜 뭘 좀 해볼까나?

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
-import Test from '../components/Test'
+import Styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
     <Head>
     </Head>
+    <div className={Styles.container}>
+      <h1>wonmo cyber school</h1>
+    </div>
     </>
   )
 }

61dc924349599705f1e87b8538180ec6b9733a62
yonmoyonmo <yonmosound@gmail.com>
2021-06-28 01:22:10 +0900

    some of admin wonmo features

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,19 +4,40 @@ import { useEffect } from "react";
 import React, { useState } from "react";
 
 const adminHome = () => {
-  const [cookie, setCookie] = useCookies(["adminToken"]);
+  const [cookie, setCookie, removeCookie] = useCookies(["adminToken"]);
   const [loginError, setLoginError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
 
-  const tokenEndpoint = endpointMania("/admin/wonmo/login");
+  const [errorString, setErrorString] = useState("");
 
-  useEffect(()=>{
-    if(cookie.adminToken){
+  const [category, setCategory] = useState("");
+  const [cateDesc, setCateDesc] = useState("");
+
+  const [notification, setNotification] = useState("");
+  const [notiText, setNotiText] = useState("");
+
+  const tokenEndpoint = endpointMania("/admin/wonmo/login");
+  const cateEndpoint = endpointMania("/admin/category");
+  const notiEndpoint = endpointMania("/admin/notification");
+
+  useEffect(() => {
+    if (cookie.adminToken) {
       setToken(cookie.adminToken);
     }
-  },[])
+  }, []);
+
+  const logoutFunction = (e) => {
+    e.preventDefault();
+    try {
+      removeCookie("adminToken");
+      window.location.reload();
+    } catch (e) {
+      window.alert(`logout 실패 : ${e}`);
+    }
+    router.push("/admin");
+  };
 
   const getAdminToken = async (e) => {
     e.preventDefault();
@@ -31,38 +52,131 @@ const adminHome = () => {
       }),
     });
     const adminTokenData = await response.json();
-    if(adminTokenData && adminTokenData.success){
+    if (adminTokenData && adminTokenData.success) {
       setCookie("adminToken", adminTokenData.data, {
-        path:"/",
+        path: "/",
         maxAge: 3600000,
       });
       window.location.reload();
-    }else{
-      setLoginError("뭔가 잘못됨")
+    } else {
+      setLoginError("뭔가 잘못됨");
     }
   };
 
+  const createCategory = async (e) => {
+    e.preventDefault();
+    const response = await fetch(cateEndpoint, {
+      method: "POST",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: category,
+        description: cateDesc,
+      }),
+    });
+    const cateData = await response.json();
+    if (cateData && cateData.success) {
+      setErrorString("카테고리 생성됨");
+    } else {
+      setErrorString("카테고리 안생성됨");
+    }
+  };
+
+  const createNotification = async (e) =>{
+    e.preventDefault();
+    const response = await fetch(notiEndpoint, {
+      method: "POST",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: notification,
+        text: notiText,
+      }),
+    });
+    const notiData = await response.json();
+    if (notiData && notiData.success) {
+      setErrorString("공지 생성됨");
+    } else {
+      setErrorString("공지 안생성됨");
+    }
+  }
+
   return (
-    <div>
-      <p>admin home</p>
-      <hr />
-      <form onSubmit={getAdminToken}>
-        <input
-          type="text"
-          value={email}
-          placeholder="admin account"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="login" />
-        {loginError && <p style={{ color: "red" }}>{loginError}</p>}
-      </form>
-    </div>
+    <>
+      {token ? (
+        <div>
+          {errorString && <p style={{ color: "red" }}>{errorString}</p>}
+          <div>
+            <p>어드민 로그인 됨</p>
+            <button onClick={logoutFunction}>로그아웃</button>
+          </div>
+          <hr/>
+          <div>
+            <label>카테고리 만들기</label>
+            <form onSubmit={createCategory}>
+              <input
+                type="text"
+                value={category}
+                placeholder="category title"
+                onChange={(e) => setCategory(e.target.value)}
+              />
+              <input
+                type="text"
+                value={cateDesc}
+                placeholder="category description"
+                onChange={(e) => setCateDesc(e.target.value)}
+              />
+              <input type="submit" value="등록" />
+            </form>
+          </div>
+
+          <hr/>
+          <div>
+            <label>공지사항 만들기</label>
+            <form onSubmit={createNotification}>
+              <input
+                type="text"
+                value={notification}
+                placeholder="noti title"
+                onChange={(e) => setNotification(e.target.value)}
+              />
+              <textarea
+                type="text"
+                value={notiText}
+                placeholder="noti description"
+                onChange={(e) => setNotiText(e.target.value)}
+              />
+              <input type="submit" value="등록" />
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>admin home</p>
+          <hr />
+          <form onSubmit={getAdminToken}>
+            <input
+              type="text"
+              value={email}
+              placeholder="admin account"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input type="submit" value="login" />
+            {loginError && <p style={{ color: "red" }}>{loginError}</p>}
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 export default adminHome;

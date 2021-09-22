d572318eb106f68ea710fe977a395a7df03eed51
yonmoyonmo <yonmosound@gmail.com>
2021-09-22 17:24:28 +0900

    disable id/pw login

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -62,59 +62,6 @@ const login = () => {
     <div className="window-body">
       <div className="window">
         <div className="title-bar">
-          <div className="title-bar-text">Login</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
-          </div>
-        </div>
-        <div className="window-body">
-          <p style={{ textAlign: "center" }}>normal login with ID/PASSWORD</p>
-          {loginError && <p style={{ color: "red" }}>{loginError}</p>}
-          <form onSubmit={handleSubmit}>
-            <div className="field-row" style={{ justifyContent: "center" }}>
-              <input
-                name="email"
-                type="email"
-                value={email}
-                placeholder="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="field-row" style={{ justifyContent: "center" }}>
-              <input
-                name="password"
-                type="password"
-                value={password}
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <br />
-            <div className="field-row" style={{ justifyContent: "center" }}>
-              <input type="submit" value="login" />
-            </div>
-            <hr />
-            <div className="field-row" style={{ justifyContent: "center" }}>
-              <p>계정이 없다면 간단히 등록할 수 있음</p>
-            </div>
-            <div className="field-row" style={{ justifyContent: "center" }}>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  router.push("/signup");
-                }}
-              >
-                <Link href="/signup">회원가입</Link>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <br />
-      <div className="window">
-        <div className="title-bar">
           <div className="title-bar-text">OAuth 2.0</div>
           <div className="title-bar-controls">
             <button aria-label="Minimize" />
@@ -125,6 +72,8 @@ const login = () => {
         <div className="window-body">
           <div className="field-row" style={{ justifyContent: "center" }}>
             <p style={{ textAlign: "center" }}>
+              구글 소셜 로그인
+              <br />
               social login with a google account
             </p>
           </div>

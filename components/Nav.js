6be2cabb4b67b196fc764f6f24cba8b9ce3f0007
yonmoyonmo <yonmosound@gmail.com>
2021-09-29 18:23:57 +0900

    fix nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-
 const Nav = () => {
   const router = useRouter();
   return (
-    <div className="window" style={{margin:"1rem"}}>
+    <div className="window" style={{ margin: "1rem" }}>
       <div className="title-bar">
         <div className="title-bar-text">Wonmo Cyber School 2</div>
         <div className="title-bar-controls">
@@ -14,19 +13,25 @@ const Nav = () => {
           <button aria-label="Close" />
         </div>
       </div>
-      <div className="window-body" style={{ textAlign: "center", padding:"1rem"}}>
-          <button>
-            <Link href="/">home</Link>
-          </button>
-          <button>
-            <Link href="/notification">notification</Link>
-          </button>
-          <button>
-            <Link href="/user/profile">your_profile</Link>
-          </button>
-          <button>
-            <Link href="/about">about</Link>
-          </button>
+      <div
+        className="window-body"
+        style={{ textAlign: "center", padding: "1rem" }}
+      >
+        <Link href="/">
+          <button>home</button>
+        </Link>
+
+        <Link href="/notification">
+          <button>notification</button>
+        </Link>
+
+        <Link href="/user/profile">
+          <button>your_profile</button>
+        </Link>
+
+        <Link href="/about">
+          <button>about</button>
+        </Link>
       </div>
     </div>
   );
